3e4eb07e6a5fde9e2934eb9cb5487cfe2c26b707
Brian Beckett <brian@worthlesscheese.com>
2014-02-26 02:24:30 +0000

    Improve naming

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -1,7 +1,7 @@
 //Load in the require.js config file
 require([
 	'require-config'
-], function(conf) {
+], function(requireConfig) {
 
 	require(['app'], function(app) {
 		// This function left deliberately blank - app executes at require-time.
